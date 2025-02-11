ad2cda259342a3239b18dabbb81ff2b846cc4ad8
Ato <ato19942@gmail.com>
2025-02-11 20:41:53 +0100

    1

--- src/components/car.vue
+++ src/components/car.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-    export default {
-                
+export default {
+    components: {
+        
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
